d9c93555ac001ba391c86e2f8656bb06f00cbcc5
Noel.b <jaeyeong.b@gmail.com>
2016-07-10 18:42:36 +0900

    Save the prisoner

--- algorithms/warmup/save-the-prisoner.py
+++ algorithms/warmup/save-the-prisoner.py
@@ -5,15 +5,9 @@ test_case = int(raw_input().strip())
 
 for i in range(test_case):
     prisoners, sweets, start = map(int,sys.stdin.readline().split())
-    cur_idx = start
-
-    for i in range(start, start + sweets):
-        if i != cur_idx:
-            if cur_idx % prisoners == 0:
-                cur_idx = 1
-            else:
-                cur_idx += 1
-        sweets -= 1
-        if sweets == 0:
-            print(cur_idx)
-            break
+    res = 0
+    if (sweets + start - 1) % prisoners == 0:
+        res = prisoners
+    else:
+        res = (sweets + start - 1) % prisoners
+    print(res)
