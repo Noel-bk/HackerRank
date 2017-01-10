018c7ea037ece9b44fbc1d327e9d83dde7bda396
Noel.b <noel.b@kakaocorp.com>
2017-01-10 14:07:51 +0900

    Calculating Volume

---
+++ java/object-oriented-programming/CalculatingVolume.java
@@ -0,0 +1,127 @@
+import java.io.IOException;
+import java.security.Permission;
+import java.util.Scanner;
+
+public class CalculatingVolume {
+
+    public static void main(String[] args) {
+        DoNotTerminate.forbidExit();
+        try {
+            Calculate cal = new Calculate();
+            int T = cal.get_int_val();
+            while (T-- > 0) {
+                double volume = 0.0;
+                int ch = cal.get_int_val();
+                if (ch == 1) {
+                    int a = cal.get_int_val();
+                    volume = Calculate.do_calc().get_volume(a);
+                } else if (ch == 2) {
+                    int l = cal.get_int_val();
+                    int b = cal.get_int_val();
+                    int h = cal.get_int_val();
+                    volume = Calculate.do_calc().get_volume(l, b, h);
+
+                } else if (ch == 3) {
+                    double r = cal.get_double_val();
+                    volume = Calculate.do_calc().get_volume(r);
+
+                } else if (ch == 4) {
+                    double r = cal.get_double_val();
+                    double h = cal.get_double_val();
+                    volume = Calculate.do_calc().get_volume(r, h); 
+                }
+                cal.output.display(volume);
+            } 
+        } catch (NumberFormatException e) {
+            System.out.print(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        } 
+    }
+}
+
+class Calculate {
+    Scanner sc = new Scanner(System.in);
+    Output output = new Output();
+
+    int get_int_val() throws IOException {
+        int input = readInt();
+        if (input <= 0) {
+            throwException();
+        }
+        return input;
+    }
+
+    int readInt() {
+        return sc.nextInt();
+    }
+
+    static Volume do_calc() {
+        return new Volume();
+    }
+
+    double get_double_val() throws IOException {
+        double input = sc.nextDouble(); 
+        if (input <= 0) {
+            throwException();
+        }
+        return input;
+    }
+
+    void throwException() {
+        throw new NumberFormatException("All the values must be positive");
+    }
+}
+
+class Volume {
+    // the volume of a cube
+    double get_volume(int a) {
+        return a * a * a;
+    }
+
+    // the volume of a cuboid
+    double get_volume(int l, int b, int h) {
+        return l * b * h;
+    }
+
+    // the volume of a hemisphere
+    double get_volume(double r) {
+        return (2 * Math.PI * r * r * r) / 3;
+    }
+
+    // the volume of a cylinder
+    double get_volume(double r, double h) {
+        return Math.PI * r * r * h;
+    }
+}
+
+class Output {
+    void display(double num) {
+        System.out.format("%.3f", num);
+    }
+}
+
+/**
+ * This class prevents the user form using System.exit(0)
+ * from terminating the program abnormally.
+ */
+class DoNotTerminate {
+
+    public static class ExitTrappedException extends SecurityException {
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
+
